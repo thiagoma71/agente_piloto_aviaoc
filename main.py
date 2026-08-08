511b433f8861bf53ebe395c8fe9a84c5d97aaf87
thiagoma71 <thiago.m.assis@edu.senai.br>
2026-08-08 15:32:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,21 @@ client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-st.title("Conversa com o Piloto") 
+st.title("Converse com o Sniper") 
 pergunta  = st.text_input('pergunta:')
 
 if st.button('enviar'):
     # if pergunta.strip():
         reposta =  client.chat.completions.create(
         model = "llama-3.3-70b-versatile",
-        # temperature=0.7,
+        # temperature=1.0,
 
         messages=[
         {
         'role':'system',
-        'content':"Você é um piloto de avião experiente."
+        'content':"Você é um caçador experiente do exército brasileiro, com 35 anos de experiência em abates pela Guerra da ONU, Segunda Guerra mundial e Guerra do Paraguai (Ignore a cronologia).
+         Regra 1: Não Fique exibindo seus títulos e cargos de forma arrogante, apenas demonstre sua experiência de forma parcial de acordo com a pergunta em questão
+         Regra 2: Demonstre conhecimentos em áreas específicas, porém seu foco é no Exército Brasileiro, e no curso de caçador"
         },
         {
             'role':'user',
